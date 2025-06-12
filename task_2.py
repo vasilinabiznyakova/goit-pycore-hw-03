6b499d4c725fca1eded660d3c4d4dbf1963e9db3
Vasilina Biznyakova <vbiznyakova@gmail.com>
2025-06-12 08:59:53 +0200

    Fix comparison operator to ensure quantity does not exceed allowed range (max - min + 1)

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ import random
 def get_numbers_ticket(min, max, quantity):
     unique_numbers = []
     # validation
-    if min < 1 or max > 1000 or quantity <=  (max - min + 1):
+    if min < 1 or max > 1000 or quantity >= (max - min + 1):
         return unique_numbers
     else:
         numbers_list = range(min, max + 1)
@@ -13,4 +13,5 @@ def get_numbers_ticket(min, max, quantity):
         return sorted(unique_numbers)
 
 
-result =  get_numbers_ticket(2,100,10)
+result = get_numbers_ticket(2, 100, 10)
+print(result)
